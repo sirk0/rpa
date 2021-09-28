3372598e5cf4d5ce9e8c4398d83d67e245f684c9
sergiy <tcymbal@gmail.com>
2021-09-29 00:02:09 +0300

    download pdfs

--- tasks.py
+++ tasks.py
@@ -2,6 +2,9 @@ from RPA.Browser.Selenium import Selenium
 from RPA.Excel.Files import Files
 from RPA.FileSystem import FileSystem
 
+import time
+from pathlib import Path
+
 browser_lib = Selenium()
 excel_lib = Files()
 file_system_lib = FileSystem()
@@ -38,9 +41,30 @@ def get_individual_investments(department):
     for row in rows:
         tds = [td.text for td in row.find_elements_by_xpath(".//td")]
         table.append(tds)
+    links = [uii.get_attribute('href') for uii in row.find_elements_by_xpath('//td[1]/a')]
+    for link in links:
+        print(link)
+        download_pdf(link)
+
     return table
 
 
+def download_pdf(link):
+    try:
+        filename = link.split('/')[-1] + '.pdf'
+        source = str(Path('~').expanduser().joinpath('Downloads').joinpath(filename))
+        file_system_lib.remove_file(source)
+        open_the_website(link)
+        locator_download = "//a[contains(text(),'Download Business Case PDF')]"
+        browser_lib.wait_until_element_is_visible(locator_download, timeout=TIMEOUT)
+        browser_lib.find_element("//a[contains(text(),'Download Business Case PDF')]").click()
+        time.sleep(15)
+        file_system_lib.copy_file(source, f"output/{filename}")
+        
+    finally:
+        browser_lib.close_browser()
+
+    
 def create_excel_worksheet(name, content):
     excel_lib.create_worksheet(name, content=content)
 
@@ -56,6 +80,7 @@ def test_rows():
 def main():
     try:
         file_system_lib.create_directory('output')
+        browser_lib.set_download_directory('output')
         open_the_website("https://itdashboard.gov/")
         departments = get_departments()
         print(departments)
